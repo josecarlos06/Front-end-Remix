0ddc591333ac3b470ae856810562d61aae820ce2
josecarlos06 <carlosmedina.vasq06@gmail.com>
2023-04-07 12:34:46 -0600

    Refacto

--- app/routes/guitarras/$url.jsx
+++ app/routes/guitarras/$url.jsx
@@ -4,7 +4,7 @@ import { UniqueData } from "~/services/api.server";
 
 export async function loader({params}){
    const {url} = params
-   const data = await UniqueData("posts",url)
+   const data = await UniqueData("guitarras",url)
    if(data.data.length === 0){
       throw new Response('',{
          status : 404,
